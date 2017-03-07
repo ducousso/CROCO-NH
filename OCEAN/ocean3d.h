7bd8257ba0ca9264044a5096a85e46db436d8079
Nicolas Ducousso <nicolas.ducousso@ifremer.fr>
2017-03-07 02:41:53 +0100

    importing the declaration and use of nhssh from Guillaume s branch

--- OCEAN/ocean3d.h
+++ OCEAN/ocean3d.h
@@ -66,7 +66,8 @@
       real nhdu(GLOBAL_2D_ARRAY,1:N,2)
       real nhdv(GLOBAL_2D_ARRAY,1:N,2)
       real nhdw(GLOBAL_2D_ARRAY,0:N,2)
-      common /ocean_wz/ wz,nhdu,nhdv,nhdw
+      real nhssh(GLOBAL_2D_ARRAY)
+      common /ocean_wz/ wz,nhdu,nhdv,nhdw,nhssh
 # endif
 
 # if defined UV_VIS4 && defined UV_MIX_GEO
